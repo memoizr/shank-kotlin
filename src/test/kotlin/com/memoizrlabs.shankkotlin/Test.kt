cece6ca88b6caf3b91272304f8300e765ac3c8ca
memoizr <memoizrlabs@gmail.com>
2016-07-06 17:04:33 +0100

    support nulls as parameters

--- src/test/kotlin/com/memoizrlabs.shankkotlin/Test.kt
+++ src/test/kotlin/com/memoizrlabs.shankkotlin/Test.kt
@@ -5,26 +5,33 @@ import com.memoizrlabs.Shank
 import com.memoizrlabs.Shank.with
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.math.BigDecimal
 import java.math.BigInteger
 
 class Test {
     @Test
     fun wrapsCorrectly() {
-        registerFactory(String::class) { -> "No param"}
-        registerNamedFactory(String::class, "one") { -> "No param"}
+        registerFactory(String::class) { -> "No param" }
+        registerNamedFactory(String::class, "one") { -> "No param" }
         assertEquals(provideGlobalSingleton<String>(), "No param")
         assertEquals(provideNew<String>(), "No param")
         assertEquals(with(scope("one")).provideSingletonFor<String>(), "No param")
         assertEquals(Shank.named("one").provideNew<String>(), "No param")
         assertEquals(Shank.named("one").provideSingletonFor<String>(), "No param")
 
-        registerFactory(String::class) { a: BigInteger -> "No param $a"}
-        registerNamedFactory(String::class, "one") { a: BigInteger -> "No param $a"}
+        registerFactory(String::class) { a: BigInteger -> "No param $a" }
+        registerNamedFactory(String::class, "one") { a: BigInteger -> "No param $a" }
         assertEquals(provideGlobalSingleton<String>(BigInteger("2")), "No param 2")
         assertEquals(provideNew<String>(BigInteger("2")), "No param 2")
         assertEquals(with(scope("one")).provideSingletonFor<String>(BigInteger("2")), "No param 2")
         assertEquals(Shank.named("one").provideNew<String>(BigInteger("2")), "No param 2")
         assertEquals(Shank.named("one").provideSingletonFor<String>(BigInteger("2")), "No param 2")
+
+        registerFactory(String::class) { a: BigInteger, b: BigDecimal -> "No param $a, $b" }
+        assertEquals(with(scope("one")).provideSingletonFor<String>(BigInteger("2"), BigDecimal(2)), "No param 2, 2")
+        registerFactory(String::class) { a: BigInteger, b: BigDecimal? -> "No param $a, $b" }
+        val bigDecimal: BigDecimal? = null
+        assertEquals(with(scope("one")).provideSingletonFor<String>(BigInteger("2"), bigDecimal), "No param 2, null")
     }
 
 }
